9ea087e8bb62c6e74fb92181b09225df605c2752
Krzysiek <krzysiekczaja@gmail.com>
2019-04-10 22:03:58 +0200

    Add addValue() method

--- src/main/java/pl/codecool/storedata/Answer.java
+++ src/main/java/pl/codecool/storedata/Answer.java
@@ -1,4 +1,24 @@
 package pl.codecool.storedata;
 
+import pl.codecool.value.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Answer {
+    List<Value> valueList;
+
+    public Answer() {
+        valueList = new ArrayList<>();
+    }
+
+    public boolean evaluateAnswerByInput(String input) {
+        for (Value value: valueList) {
+            
+        }
+    }
+
+    public void addValue(Value value) {
+        valueList.add(value);
+    }
 }
