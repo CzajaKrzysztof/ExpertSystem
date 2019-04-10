27087c2fc86dc2c417000a91c1691d5a31ece1d6
Krzysiek <krzysiekczaja@gmail.com>
2019-04-10 18:21:33 +0200

    Add methods in xmlparser abstract class

--- src/main/java/pl/codecool/xmlparser/XMLParser.java
+++ src/main/java/pl/codecool/xmlparser/XMLParser.java
@@ -1,10 +1,34 @@
 package pl.codecool.xmlparser;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 public abstract class XMLParser {
+    private Document doc;
 
-    public XMLParser() {
-
+    public void loadXmlDocument(String xmlPath) {
+        try {
+            File inputFile = new File(xmlPath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
-    public abstract void loadXmlDocument(String xmlPath);
+    public Document getDoc() {
+        return doc;
+    }
 }
