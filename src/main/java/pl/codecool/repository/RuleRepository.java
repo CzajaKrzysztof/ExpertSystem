b93c9b0b72b5ddd065af717419cd5cab43be2662
Krzysiek <krzysiekczaja@gmail.com>
2019-04-10 21:05:06 +0200

    Add RuleRepository inner class iterator

--- src/main/java/pl/codecool/repository/RuleRepository.java
+++ src/main/java/pl/codecool/repository/RuleRepository.java
@@ -1,4 +1,41 @@
 package pl.codecool.repository;
 
+import pl.codecool.storedata.Question;
+
+import java.util.*;
+
 public class RuleRepository {
+    Map<String, Question> ruleRepository;
+
+    public RuleRepository() {
+        ruleRepository = new TreeMap<>();
+    }
+
+    public Iterator<Question> getIterator() {
+        return new QuestionIterator();
+    }
+
+    public class QuestionIterator implements Iterator {
+//        int index = 0;
+//        Object[] keys = ruleRepository.keySet().toArray();
+        Iterator<String> keyIterator = ruleRepository.keySet().iterator();
+
+        @Override
+        public boolean hasNext() {
+//            if (index < keys.length) {
+//                return true;
+//            }
+//            return false;
+            return keyIterator.hasNext();
+        }
+
+        @Override
+        public Object next() {
+            if (this.hasNext()) {
+//                return ruleRepository.get(keys[index++]);
+                return ruleRepository.get(keyIterator.next());
+            }
+            return null;
+        }
+    }
 }
