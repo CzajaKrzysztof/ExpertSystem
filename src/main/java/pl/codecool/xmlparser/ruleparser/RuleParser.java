c084409c421b046fe4cf4b245011c4e3d7aa0794
Krzysiek <krzysiekczaja@gmail.com>
2019-04-10 19:29:33 +0200

    Add empty getRuleRepository() method for further implementation

--- src/main/java/pl/codecool/xmlparser/ruleparser/RuleParser.java
+++ src/main/java/pl/codecool/xmlparser/ruleparser/RuleParser.java
@@ -1,6 +1,16 @@
 package pl.codecool.xmlparser.ruleparser;
 
+import pl.codecool.repository.RuleRepository;
 import pl.codecool.xmlparser.XMLParser;
 
 public class RuleParser extends XMLParser {
+    RuleRepository ruleRepository;
+
+    public RuleParser() {
+        getRuleRepository();
+    }
+
+    public RuleRepository getRuleRepository () {
+
+    }
 }
