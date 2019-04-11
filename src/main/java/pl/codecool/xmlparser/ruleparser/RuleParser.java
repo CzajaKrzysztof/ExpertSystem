43bd3176a72e3dfe9fd41ec276c60f71b74a396c
Krzysiek <krzysiekczaja@gmail.com>
2019-04-11 19:04:54 +0200

    Add basic xml parser functionality

--- src/main/java/pl/codecool/xmlparser/ruleparser/RuleParser.java
+++ src/main/java/pl/codecool/xmlparser/ruleparser/RuleParser.java
@@ -1,5 +1,8 @@
 package pl.codecool.xmlparser.ruleparser;
 
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import pl.codecool.repository.RuleRepository;
 import pl.codecool.xmlparser.XMLParser;
 
@@ -10,7 +13,23 @@ public class RuleParser extends XMLParser {
         getRuleRepository();
     }
 
-    public RuleRepository getRuleRepository () {
-
+    private RuleRepository getRuleRepository() {
+        NodeList ruleList = getDoc().getElementsByTagName("Rule");
+        for (int rule = 0; rule < ruleList.getLength(); rule++) {
+            Node ruleNode = ruleList.item(rule);
+            if (ruleNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element ruleElement = (Element) ruleNode;
+                System.out.println("Question:\t" + ruleElement.getFirstChild().getNextSibling().getTextContent());
+                NodeList selectionList = ruleElement.getElementsByTagName("Selection");
+                for (int selection = 0; selection < selectionList.getLength(); selection++) {
+                    Node selectionNode = selectionList.item(selection);
+                    if (selectionNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element selectionElement = (Element) selectionNode;
+                        System.out.println("Sel val:\t" + selectionElement.getAttribute("value"));
+                        System.out.println("Ans val:\t" + selectionElement.getFirstChild().getNextSibling().getAttributes().getNamedItem("value").getNodeValue());
+                    }
+                }
+            }
+        }
     }
 }
