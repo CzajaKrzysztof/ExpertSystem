754d7e157346dafb1ee164b6669030a1e60b8910
Krzysiek <krzysiekczaja@gmail.com>
2019-04-12 21:40:20 +0200

    Add Fact class structure with all methodes

--- src/main/java/pl/codecool/storedata/Fact.java
+++ src/main/java/pl/codecool/storedata/Fact.java
@@ -1,4 +1,33 @@
 package pl.codecool.storedata;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Fact {
+    private String id;
+    private String description;
+    private Map<String, Boolean> evals;
+
+    public Fact(String id, String description) {
+        evals = new LinkedHashMap<>();
+        this.id = id;
+        this.description = description;
+    }
+
+    public Set<String> getIdSet() {
+        return evals.keySet();
+    }
+
+    public void setFactValueById(String id, Boolean value) {
+        evals.put(id, value);
+    }
+
+    public boolean getValueById(String id) {
+        return evals.get(id);
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
