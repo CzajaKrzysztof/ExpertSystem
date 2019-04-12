3210d393aab2c1be3e2c39f33b0578adff45d959
Krzysiek <krzysiekczaja@gmail.com>
2019-04-12 21:51:39 +0200

    Add FactRepository class body

--- src/main/java/pl/codecool/repository/FactRepository.java
+++ src/main/java/pl/codecool/repository/FactRepository.java
@@ -1,4 +1,39 @@
 package pl.codecool.repository;
 
+import pl.codecool.storedata.Fact;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class FactRepository {
+    private List<Fact> facts;
+    Iterator<Fact> factIterator;
+
+    public FactRepository() {
+        facts = new ArrayList<>();
+        factIterator = new FactIterator();
+    }
+
+    public void addFact(Fact fact) {
+        facts.add(fact);
+    }
+
+    public Iterator<Fact> getFactIterator() {
+        return factIterator;
+    }
+
+    private class FactIterator implements Iterator<Fact> {
+        int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return (index < facts.size());
+        }
+
+        @Override
+        public Fact next() {
+            return facts.get(index++);
+        }
+    }
 }
