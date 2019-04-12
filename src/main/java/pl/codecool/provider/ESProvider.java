596878393da849a8b125a3e1d61ddf93db3d5d2a
Krzysiek <krzysiekczaja@gmail.com>
2019-04-12 18:14:47 +0200

    Add sceleton for every method

--- src/main/java/pl/codecool/provider/ESProvider.java
+++ src/main/java/pl/codecool/provider/ESProvider.java
@@ -4,9 +4,23 @@ import pl.codecool.xmlparser.factparser.FactParser;
 import pl.codecool.xmlparser.ruleparser.RuleParser;
 
 public class ESProvider {
+    FactParser factParser;
+    RuleParser ruleParser;
 
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
+        this.factParser = factParser;
+        this.ruleParser = ruleParser;
+    }
+
+    public void collectAnswers(){
 
     }
 
+    public boolean getAnswerByQuestion(String questionId) {
+        return false;
+    }
+
+    public String evaluate() {
+
+    }
 }
