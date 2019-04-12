05bdf4af99dca2c133b20dc7614d3e0773544045
Krzysiek <krzysiekczaja@gmail.com>
2019-04-12 17:23:54 +0200

    Add ESProvider object creation in Main cass and base of ESProvider constructor

--- src/main/java/pl/codecool/Main.java
+++ src/main/java/pl/codecool/Main.java
@@ -1,7 +1,11 @@
 package pl.codecool;
 
+import pl.codecool.provider.ESProvider;
+import pl.codecool.xmlparser.factparser.FactParser;
+import pl.codecool.xmlparser.ruleparser.RuleParser;
+
 public class Main {
     public static void main(String[] args) {
-
+        ESProvider provider = new ESProvider(new FactParser(), new RuleParser());
     }
 }
--- src/main/java/pl/codecool/provider/ESProvider.java
+++ src/main/java/pl/codecool/provider/ESProvider.java
@@ -1,8 +1,11 @@
 package pl.codecool.provider;
 
+import pl.codecool.xmlparser.factparser.FactParser;
+import pl.codecool.xmlparser.ruleparser.RuleParser;
+
 public class ESProvider {
 
-    public ESProvider() {
+    public ESProvider(FactParser factParser, RuleParser ruleParser) {
 
     }
 
