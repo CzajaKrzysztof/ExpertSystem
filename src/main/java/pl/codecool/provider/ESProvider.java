410fccdb6559f4d0556f3bed456c00c28a3abd3c
Krzysiek <krzysiekczaja@gmail.com>
2019-04-12 21:26:55 +0200

    Agg methodes signatures

--- src/main/java/pl/codecool/provider/ESProvider.java
+++ src/main/java/pl/codecool/provider/ESProvider.java
@@ -1,15 +1,16 @@
 package pl.codecool.provider;
 
+import pl.codecool.repository.FactRepository;
+import pl.codecool.repository.RuleRepository;
 import pl.codecool.xmlparser.factparser.FactParser;
 import pl.codecool.xmlparser.ruleparser.RuleParser;
 
 public class ESProvider {
-    FactParser factParser;
-    RuleParser ruleParser;
-
+    RuleRepository ruleRepository;
+    FactRepository factRepository;
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
-        this.factParser = factParser;
-        this.ruleParser = ruleParser;
+        ruleRepository = ruleParser.getRuleRepository();
+        factRepository = factParser.getFactRepository();
     }
 
     public void collectAnswers(){
